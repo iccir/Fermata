2435f4164fb83520350bddfd01956629c23c1efc
iccir <1097294+iccir@users.noreply.github.com>
2019-12-10 03:59:09 -0800

    Bump version and copyright

--- Source/Launcher_main.m
+++ Source/Launcher_main.m
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2017-2018 Ricci Adams
+    Copyright (c) 2017-2019 Ricci Adams
 
     Permission is hereby granted, free of charge, to any person obtaining
     a copy of this software and associated documentation files (the
